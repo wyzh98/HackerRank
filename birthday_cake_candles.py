ad5e5a2bad29750214dbee446de2868d99e80651
Wang Yizhuo <32894577+wyzh98@users.noreply.github.com>
2020-05-25 01:00:39 +0800

    Update birthday_cake_candles.py
    Add some discriptions

--- birthday_cake_candles.py
+++ birthday_cake_candles.py
@@ -9,8 +9,8 @@ import sys
 # Complete the birthdayCakeCandles function below.
 def birthdayCakeCandles(ar_count, ar):
 '''
-In previous version, it times out because I did not store the maxar previouly,
-but calculate it at each loop, which is 'if ar[i] == max(ar)', and it is time consuming.
+In previous version, it times out in large-scale tests because I did not store the max(ar) before the loop,
+but at each loop, which is 'if ar[i] == max(ar)', and it is time consuming.
 '''
     cnt = 0
     maxar = max(ar)
